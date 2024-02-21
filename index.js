30aeead9ae5a808e42f43168606d482cdfdd68cc
Md Jahid Hasan <jahidhasan658560@gmail.com>
2024-02-21 10:01:23 +0600

    pin mached complete

--- index.js
+++ index.js
@@ -20,6 +20,48 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     displayField.value = pin
 });
 
-document.getElementById('calculetor').addEventListener('click', function(){
-    
+document.getElementById('calculetor').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typeNumberField = document.getElementById('typed-numbers');
+    const previousNumber = typeNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typeNumberField.value = '';
+        }
+        else if (number ==='<'){
+            const digits = previousNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typeNumberField.value = remainingDigits;
+        }
+    }
+    else{
+       
+        const newTypeNumber = previousNumber + number;
+        typeNumberField.value = newTypeNumber;
+    }
+});
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typeNumberField = document.getElementById('typed-numbers');
+    const typeNumber = typeNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('notify-success');
+    const pinFailMessage = document.getElementById('notify-wrong');
+
+    if(typeNumber === currentPin){
+       
+        pinSuccessMessage.style.display = 'block'
+        pinFailMessage.style.display = 'none'
+
+    }
+    else{
+       
+        pinFailMessage.style.display = 'block'
+        pinSuccessMessage.style.display = 'none'
+    }
+
 })
